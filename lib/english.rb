96f9a5ed4c26062356e9ad010b60c74e725172ef
7rans <transfire@gmail.com>
2010-06-05 12:24:15 -0400

    add infinitve.eb to english.rb and update HISTORY

--- lib/english.rb
+++ lib/english.rb
@@ -1,15 +1,20 @@
 require 'english/class'
 
 require 'english/conjunction'
+require 'english/infinitive'
 require 'english/inflect'
-require 'english/metaphone'
 require 'english/namecase'
+
 require 'english/numeral'
 require 'english/ordinal'
+
 require 'english/dresner'
 require 'english/jumble'
+
+require 'english/metaphone'
 require 'english/soundex'
 require 'english/porter'
+
 require 'english/words'
 require 'english/censor'
 
